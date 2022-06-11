81c0a180221c86eb40eaaddc1dafdd982c73c9db
Cody Murray <cdillon.murray@yahoo.com>
2022-06-11 18:47:22 -0500

    updaed controllers

--- controllers/thought-controller.js
+++ controllers/thought-controller.js
@@ -4,21 +4,21 @@ const thoughtController = {
   getAllThoughts(req, res) {
     Thought.find({})
       .populate({
-        path: "user",
+        path: "reactions",
         select: "-__v",
       })
       .select("-__v")
       .sort({ _id: -1 })
-      .then((dbThoughtData) => res.json(dbThoughtData))
+      .then(userData => res.json(userData))
       .catch((err) => {
         console.log(err);
-        res.status(500).json(err);
+        res.status(400).json(err);
       });
   },
   getThoughtById({ params }, res) {
     Thought.findOne({ _id: params.id })
       .populate({
-        path: "user",
+        path: "reactions",
         select: "-__v",
       })
       .select("-__v")
@@ -29,25 +29,23 @@ const thoughtController = {
         res.status(500).json(err);
       });
   },
-  createThought({ params, body }, res) {
+  createThought({ body }, res) {
     Thought.create(body)
       .then(({ _id }) => {
         return User.findOneAndUpdate(
-          { username: body.username },
-          { $push: { thoughts: _id } },
+          { _id: body.userId },
+          { $push: { thoughts: _id }},
           { new: true }
         );
       })
-      .then((dbUserData) => {
-        if (!dbUserData) {
-          res
-            .status(404)
-            .json({ message: "No user found" });
+      .then(userData => {
+        if (!userData) {
+          res.status(404).json({ message: 'No user found with this id!' });
           return;
         }
-        res.json(dbUserData);
+        res.json(userData);
       })
-      .catch((err) => res.json(err));
+      .catch(err => res.json(err));
   },
   addReaction({ params, body }, res) {
     Thought.findOneAndUpdate(
